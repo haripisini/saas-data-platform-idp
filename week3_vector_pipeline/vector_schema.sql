2241c6a661c289722fc99b38f68d277df62735e1
Hari Pisini <yourgmail@gmail.com>
2026-03-11 01:02:40 +0530

    Update week 3 and week 4 progress

--- week3_vector_pipeline/vector_schema.sql
+++ week3_vector_pipeline/vector_schema.sql
@@ -1,11 +1,6 @@
--- Enable pgvector extension
-CREATE EXTENSION IF NOT EXISTS vector;
-
--- Table for storing document embeddings
-
 CREATE TABLE document_embeddings (
     id SERIAL PRIMARY KEY,
     document_id TEXT,
     document_text TEXT,
     embedding VECTOR(1536)
-);git
+);
